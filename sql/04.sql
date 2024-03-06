75f50d63099ff2b53d9e4609997b5816f773fdac
rachelHoman <rachelmarie001@aol.com>
2024-03-05 20:33:36 -0800

    fixed problem 4

--- sql/04.sql
+++ sql/04.sql
@@ -10,7 +10,7 @@ JOIN film_actor USING (actor_id)
 JOIN film USING (film_id)
 JOIN film_category USING (film_id)
 JOIN category USING (category_id)
-WHERE category.name='Children'
+WHERE name = 'Children'
 EXCEPT
 SELECT first_name, last_name
 FROM actor
@@ -18,5 +18,5 @@ JOIN film_actor USING (actor_id)
 JOIN film USING (film_id)
 JOIN film_category USING (film_id)
 JOIN category USING (category_id)
-WHERE category.name='Horror'
-ORDER BY last_name, first_name;
+WHERE name = 'Horror'
+ORDER BY last_name;
